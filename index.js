f0f5a2076c541ccd0ff6c35d0838be4d390c9e64
Kevin McAteer <kmcateer926@gmail.com>
2020-11-04 21:08:00 -0500

    readme file is populating

--- index.js
+++ index.js
@@ -1,104 +1,146 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // const writeFileAsync = util.promisify(fs.writeFile);
 
 const markDown = "demo/README-demo.md";
 // array of questions for user
-const promptUser = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "title",
-        message: "What is your title of your project?",
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "give me a description of your project",
-      },
-      {
-        type: "input",
-        name: "homepage",
-        message: "What is your project homepage?",
-      },
-      {
-        type: "input",
-        name: "testInstructions",
-        message: "Are there specific test instructions (if no, press enter)?",
-      },
-      {
-        type: "input",
-        name: "install",
-        message: "How do you install your application?",
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: "describe the usage of your project",
-      },
-      {
-        type: "input",
-        name: "tech",
-        message: "What technology did you use?",
-      },
-      {
-        type: "input",
-        name: "url",
-        message: "Please share the complete URL to access your project.",
-      },
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "give me a description of your project",
+  },
+  {
+    type: "input",
+    name: "author",
+    message: "who is the author of this project?",
+  },
+  {
+    type: "input",
+    name: "testInstructions",
+    message: "Are there specific test instructions (if no, press enter)?",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "How do you install your application?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "describe the usage of your project",
+  },
+  {
+    type: "input",
+    name: "technology",
+    message: "What technology did you use?",
+  },
+  {
+    type: "input",
+    name: "url",
+    message: "Please share the complete URL to access your project.",
+  },
 
-      {
-        type: "input",
-        name: "contributers",
-        message: "List your contributors",
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "What is your github username?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is your email address?",
-      },
-      {
-        type: "list",
-        name: "license",
-        message: "Which license would you like to use?",
-        choices: [
-          "MIT",
-          "Mozilla Public License",
-          "Apache License",
-          "GNU AGPLv3",
-          "GPL",
-        ],
-      },
-    ])
+  {
+    type: "input",
+    name: "contributors",
+    message: "List your contributors",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "phone",
+    message: "What is your phone number?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license would you like to use?",
+    choices: [
+      "MIT",
+      "Mozilla Public License",
+      "Apache License",
+      "GNU AGPLv3",
+      "GPL",
+    ],
+  },
+];
+
+inquirer.prompt(questions)
 
     .then(function (data) {
+      const fileName = "testREADME.md";
       console.log(data);
-      writeMarkdown(markDown, Data);
+      writeToFile(fileName, data);
     });
-};
-// function to write README file
-function writeMarkdown(markDown, data) {
-  fs.writeFile(markDown, generateMarkdown(data), null, function (err) {
+
+
+
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, generateMarkdown(data), null, function (err) {
     if (err) {
       return console.log(err);
     }
-    console.log("successfully wrote README.md");
+    console.log("DONE");
   });
 }
+// .then(function (data) {
+//   console.log(data);
+//   writeMarkdown(markDown, Data);
+// });
 
-prompUser();
+// function to write README file
+// function writeMarkdown(markDown, data) {
+//   fs.writeFile(markDown, generateMarkdown(data), null, function (err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log("successfully wrote README.md");
+//   });
+// }
 
-// function to initialize program
-function init() {}
+// function writeFile(fileName, data) {
+//   console.log("indside writefile", data);
+//   //here is where the logic is placed - detailed steps the function will make
+//   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+// }
 
-// function call to initialize program
-init();
+// // const writeFileAsync = util.promisify(writeToFile);
+
+// // prompUser();
+
+// // function to initialize program
+// function init() {
+//   inquirer
+//     .prompt(questions)
+
+//     .then((data) => {
+//       console.log("inside init", data);
+//       //   function to write README file
+//       //   writeFile("README.md", generateMarkdown({ ...responses }));
+//       writeFile("README.md", generateMarkdown(data));
+//       console.log("after writefile");
+//     });
+// }
+
+// // function call to initialize program
+// init();
--- testREADME.md
+++ testREADME.md
@@ -1,9 +1,5 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `### ${data.title}
-  ![badge](https://img.shields.io/badge/${encodeURI(
-    data.license
-  )}-License-<color>)
+### sldkfj
+  ![badge](https://img.shields.io/badge/MIT-License-<color>)
   Table of Contents
   * [Description](#description)
   * [Author](#author)
@@ -20,40 +16,34 @@ function generateMarkdown(data) {
   * [Questions](#questions)
   
   ### Title
-  ${data.title}
+  sldkfj
   ### Description
-  ${data.description}
+  lsdkfj
   ### Author
-  ${data.author}
+  lsdkfj
   ### Test Instructions
-  ${data.testInstructions}
+  
   ### Installation
-  ${data.install}
+  sldfkj
   ### Usage
-  ${data.usage}
+  lsdfkj
   ### Technology
-  ${data.technology}
+  lsdkfj
   ### Url
-  ${data.url}
+  lsdkfj
   ### Contributors
-  ${data.contributors}
+  lsdkfj
    ### Github 
-  ${data.github}
+  lsdkfj
   ### Email
-  ${data.email}
+  lsdkfj
   ### Phone
-  ${data.phone}
+  lsdfkj
   ### License
-  ![badge](https://img.shields.io/badge/${encodeURI(
-    data.license
-  )}-License-<color>)
-  ${data.license}
+  ![badge](https://img.shields.io/badge/MIT-License-<color>)
+  MIT
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-  © 2020 ${data.author}.  All Rights Reserved.
+  © 2020 lsdkfj.  All Rights Reserved.
   ### Questions
-  For additional questions, reach me at ${data.email} ${data.phone}
-`;
-}
-
-module.exports = generateMarkdown;
+  For additional questions, reach me at lsdkfj lsdfkj
